854db258c6f524f1fc22f73ea0db18da7c0c9151
BuildTools <arobine.rbx@gmail.com>
2017-06-22 22:50:59 +0200

    Alex

---
+++ TOINOU/index.php
@@ -0,0 +1,33 @@
+<html>
+	<head>
+		<title>WantonGraph</title>
+		<meta charset="UTF-8"/>
+		<link href="./css/css.css" rel="stylesheet" type="text/css">
+		
+		<link rel="stylesheet" href="./bootstrap-3.3.7-dist/css/bootstrap.min.css">
+		<script src="./jquery-3.2.1.min.js"></script>
+		<script src="./bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		
+		<link href="./bootstrap3-editable/css/bootstrap-editable.css" rel="stylesheet">
+		<script src="./bootstrap3-editable/js/bootstrap-editable.js"></script>
+		
+		<link href="./bootstrap-toggle.min.css" rel="stylesheet">
+		<script src="./bootstrap-toggle.min.js"></script>
+	</head>
+	<body>
+		<nav class="navbar navbar-default" id="navbartt" style="font-size: 150%;">
+		  <div class="container-fluid">
+			<div class="navbar-header">
+			  <a class="navbar-brand" href="./index.php"><b>WantonGraph</b></a>
+			</div>
+			<ul class="nav navbar-nav">
+			  <li id="liacc"><a href="./index.php">Accueil</a></li>
+			  <li id="lieno"><a href="./eno.php">Enocean</a></li>
+			  <li id="lirfi"><a href="./rfid.php">RFID</a></li>
+			  <!--<li id="licam"><a href="./cams.php">Caméras</a></li>-->
+			</ul>
+		  </div>
+		</nav>
+	</body>
+</html>
